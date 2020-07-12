8829470326e0678b82c73453c9d4f41a04fe51f5
André Schneider <andre.cristofolini@fluig,com>
2020-07-12 18:48:37 -0300

    Initial dependencies
    - Includes devtools, gson, junit and h2.

---
+++ src/main/java/com/southsystem/desafiobackarquivo/DesafioBackArquivoApplication.java
@@ -0,0 +1,13 @@
+package com.southsystem.desafiobackarquivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackArquivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackArquivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/southsystem/desafiobackarquivo/DesafioBackArquivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southsystem.desafiobackarquivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackArquivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
